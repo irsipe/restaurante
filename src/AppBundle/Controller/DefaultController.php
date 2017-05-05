b70c74e1959f793243a3d8ab3e4f927565338963
Pedro Diaz Leiva <pdiazl62@gmail.com>
2017-05-05 08:58:22 +0200

    Ficheros de mantenimiento

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,12 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
+     /**
+     * @Route("/tablas", name="tablas")
+     */
+    public function Mantenimiento(Request $request)
+    {
+      return $this->render('tablas.html.twig' );
+    }
+    
 }
