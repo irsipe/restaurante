746e690e4068c1369dede7bd40c0be3e16362ad8
josel567 <joseluisgarcia567@gmail.com>
2017-05-09 11:23:19 +0200

    Merge origin/master
    Conflicts:
    	app/Resources/views/base.html.twig

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,45 +7,58 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        $em = $this->getDoctrine()->getEntityManager();
+        $db = $em->getConnection();
+
+        $query = "select * from menu join lineamenu using(idmenu) where fecha= '20170509' and idcategoria=1;";
+        $stmt = $db->prepare($query);
+        $params = array();
+        $stmt->execute($params);
+        $po1 = $stmt->fetchAll();
+        
+        $query = "select * from menu join lineamenu using(idmenu) where fecha= '20170509' and idcategoria=2;";
+        $stmt = $db->prepare($query);
+        $params = array();
+        $stmt->execute($params);
+        $po2 = $stmt->fetchAll();      
+        $query = "select * from menu join lineamenu using(idmenu) where fecha= '20170509' and idcategoria=3;";
+        $stmt = $db->prepare($query);
+        $params = array();
+        $stmt->execute($params);    
+        $po3 = $stmt->fetchAll();
+        
+
+        return $this->render('default/index.html.twig', [                   
+            "menu1"=> $po1,"menu2"=> $po2,"menu3"=> $po3,
         ]);
     }
-     /**
+
+    /**
      * @Route("/tablas", name="tablas")
      */
-    public function Mantenimiento(Request $request)
-    {
-      return $this->render('tablas.html.twig' );
+    public function Mantenimiento(Request $request) {
+        return $this->render('tablas.html.twig');
     }
-    
+
     /**
      * @Route("/admin")
      */
-    public function adminAction()
-    {
+    public function adminAction() {
         return new Response('<html><body>Admin page!</body></html>');
     }
-    
+
     /**
      * @Route("/dondestamos", name="dondeestamos")
      */
-    
-    public function dondeEstamos()
-    {
-         return $this->render('dondeestamos.html.twig' );
+    public function dondeEstamos() {
+        return $this->render('dondeestamos.html.twig');
     }
-    
-    
- 
-    
-  
+
 }
